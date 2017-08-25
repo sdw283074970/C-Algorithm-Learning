24557bdc893667d09a57a16fa0a337e39e06b56b
sdw283074970 <downwes@gmail.com>
2017-08-25 19:12:28 -0400

    Update STDIN.cs

--- STDIN.cs
+++ STDIN.cs
@@ -1,7 +1,11 @@
 //读取int
 int n = Convert.ToInt32(Console.ReadLine());
+
 //将string类型的标准输入转化为int[]类型数组读取
 int[] s = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), Int32.Parse);
 
 //将int[]类型数组转化为string[]类型数组
 string[] stringresult = intresult.Select(i=>i.ToString()).ToArray();
+
+//字符转ASCII
+int n = (int)(char);
