96d1d88c8268801e1ae71cb7766d31fe3a2df2d1
sdw283074970 <downwes@gmail.com>
2017-09-01 21:04:14 -0400

    Update STDIN.cs

--- STDIN.cs
+++ STDIN.cs
@@ -9,3 +9,17 @@ string[] stringresult = intresult.Select(i=>i.ToString()).ToArray();
 
 //字符转ASCII
 int n = (int)(char);
+
+//生产N位数的随机数组合
+private static char[] constant = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',
+                                  'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+                                  'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L',
+                                  'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+public static string GenerateRandomNumber(int Length) {
+    System.Text.StringBuilder newRandom = new System.Text.StringBuilder(62);
+    Random rd = new Random();
+    for (int i = 0; i < Length; i++) {
+        newRandom.Append(constant[rd.Next(62)]);
+    }
+    return newRandom.ToString();
+}
