0f371e9db31e5e0235c1b9bb074ceccf561dcf65
sdw283074970 <downwes@gmail.com>
2017-09-17 18:24:16 -0400

    Update 494. Target Sum.cs

--- 494. Target Sum.cs
+++ 494. Target Sum.cs
@@ -1,4 +1,5 @@
 //Problem discription: https://leetcode.com/problems/target-sum/description/
+//Could be simplifed, not fixed yet
 
 public class Solution {
     public int FindTargetSumWays(int[] nums, int S) {
@@ -9,23 +10,45 @@ public class Solution {
         if (S > sum || (S + sum) % 2 > 0) {
             return 0;
         }
-        if (S == 0) {
-            return 1;
-        }
         int target = (S + sum) / 2;
         int[,] dp = new int[nums.Length, target + 1];
+        if (sum == 0 && target == 0) {
+            dp[0, 0] = 1;
+            for (int i = 1; i < nums.Length; i++) {
+                dp[i, 0] = dp[i - 1, 0] + dp[i - 1, 0] + 1; 
+            }
+            return dp[nums.Length - 1, 0] + 1;
+        }
+        if (target == 0 && nums.Length != 1) {
+            dp[0, 0] = 1;
+            for (int i = 1 ; i < nums.Length; i++) {
+                if (nums[i] == 0) {
+                    dp[i, 0] = dp[i - 1, 0] + dp[i - 1, 0] + 1;
+                }
+                else {
+                    dp[i, 0] = dp[i - 1, 0];
+                }
+            }
+            return dp[nums.Length - 1, 0] + 1;
+        }
+        Array.Sort(nums);
+        Array.Reverse(nums);
         for (int i = 0; i < nums.Length; i++) {
             dp[i, 0] = 1;
         }
         for (int i = 1; i < target + 1; i++) {
             if (i == nums[0]) {
                 dp[0, i] = 1;
+                
             }
         }
         for (int i = 1; i < nums.Length; i++) {
-            for (int j = 1; j < target + 1; j++) {
-                if (i - nums[i] >= 0) {
+            for (int j = 1; j <= target; j++) {
+                if (j - nums[i] >= 0) {
                     dp[i, j] = dp[i - 1, j] + dp[i -1, j -nums[i]];
+                }
+                else if (nums[i] == 0) {
+                    dp[i, j] = dp[i - 1, j] * 3;
                 }
                 else {
                     dp[i ,j] = dp[i -1, j];
